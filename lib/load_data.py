569751994091513bcc09ea3b277dd0472dea7996
Maxime-Tong <1697804180@qq.com>
2024-07-22 16:09:29 +0800

    remove redundant configs

--- lib/load_data.py
+++ lib/load_data.py
@@ -1,53 +1,24 @@
 import numpy as np
 
-from .load_llff import load_llff_data
 from .load_blender import load_blender_data
-from .load_nsvf import load_nsvf_data
-from .load_blendedmvs import load_blendedmvs_data
-from .load_tankstemple import load_tankstemple_data
-from .load_deepvoxels import load_dv_data
-from .load_co3d import load_co3d_data
-from .load_nerfpp import load_nerfpp_data
 
 
 def load_data(args):
 
     K, depths = None, None
     near_clip = None
+    if args.dataset_type == 'blender':
+        images, poses, render_poses, hwf, i_split = load_blender_data(args.datadir, args.half_res, args.testskip)
+        print('Loaded blender', images.shape, render_poses.shape, hwf, args.datadir)
+        i_train, i_val, i_test = i_split
 
-    if args.dataset_type == 'llff':
-        images, depths, poses, bds, render_poses, i_test = load_llff_data(
-                args.datadir, args.factor, args.width, args.height,
-                recenter=True, bd_factor=args.bd_factor,
-                spherify=args.spherify,
-                load_depths=args.load_depths,
-                movie_render_kwargs=args.movie_render_kwargs)
-        hwf = poses[0,:3,-1]
-        poses = poses[:,:3,:4]
-        print('Loaded llff', images.shape, render_poses.shape, hwf, args.datadir)
-        if not isinstance(i_test, list):
-            i_test = [i_test]
+        near, far = 2., 6.
 
-        if args.llffhold > 0:
-            print('Auto LLFF holdout,', args.llffhold)
-            i_test = np.arange(images.shape[0])[::args.llffhold]
-
-        i_val = i_test
-        i_train = np.array([i for i in np.arange(int(images.shape[0])) if
-                        (i not in i_test and i not in i_val)])
-
-        print('DEFINING BOUNDS')
-        if args.ndc:
-            near = 0.
-            far = 1.
-        else:
-            near_clip = max(np.ndarray.min(bds) * .9, 0)
-            _far = max(np.ndarray.max(bds) * 1., 0)
-            near = 0
-            far = inward_nearfar_heuristic(poses[i_train, :3, 3])[1]
-            print('near_clip', near_clip)
-            print('original far', _far)
-        print('NEAR FAR', near, far)
+        if images.shape[-1] == 4:
+            if args.white_bkgd:
+                images = images[...,:3]*images[...,-1:] + (1.-images[...,-1:])
+            else:
+                images = images[...,:3]*images[...,-1:]
     else:
         raise NotImplementedError(f'Unknown dataset type {args.dataset_type} exiting')
 
